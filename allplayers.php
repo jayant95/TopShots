0ebacc1090ca0c3c365cc1271294d6c8b3ee0d0a
Unknown <yuhanz@sfu.ca>
2018-04-10 02:27:17 -0700

    update allplayers page

--- allplayers.php
+++ allplayers.php
@@ -26,16 +26,32 @@
     <?php
       require("includes/header.php");
       $id=$_GET['id'];
+      $title='';
+      if($id==1){
+        $title="Points";
+      }else if($id==2){
+        $title="Assists";
+      }else if($id==3){
+        $title="Rebounds";
+      }else if($id==4){
+        $title="Steals";
+      }else if($id==5){
+        $title="Blocks";
+      }
     ?>
   <div class="page-content">
 
     <form action="" method="post">
 
 
-    <h1 class="allplayers">Season Leaders</h1>
 
+    <h1 class="allplayers">Season Leaders</h1>
+   
     <div class="allplayers">
-      <h2 class="allplayers">Points Per Game</h2>
+      <?php
+
+      echo '<h2 class="allplayers">'.$title.' Per Game</h2>';
+      ?>
       <table class="topplayer">
 
         <tr><th class="playername">Player</th><th class="playerstat">Points</th></tr>
