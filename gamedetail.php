fc76b6fb6134c852ef911fc804041ef389ee9bc0
Unknown <yuhanz@sfu.ca>
2018-04-10 00:12:16 -0700

    style gamedetail page

--- gamedetail.php
+++ gamedetail.php
@@ -50,75 +50,80 @@
   <body>
     <?php
       require("includes/header.php");
+
     ?>
 
-
-	<form action="" method="post">
-
-	<?php
-		while($stmt->fetch()){
-			echo "<h1>".$hometeam."    ".$hometeamScore."   :  ".$guestteamScore."    ".$guestTeam."</h1>";
-		}
-	?>
-
-	<table>
-		<tr><th>Player Name</th><th>Points</th><th>Assists</th><th>Rebounds</th><th>Playtime</th></tr>
-		<?php
-
-			$query2="SELECT playerName,points,assists,rebounds,playtime FROM gameDetail WHERE gameID=?";
-			$stmt2=$db->prepare($query2);
-			$stmt2->bind_param('i',$gameID);
-			$stmt2->execute();
-			$stmt2->store_result();
-			$result2=$stmt2->bind_result($playerName,$points,$assists,$rebounds,$playtime);
-			while($stmt2->fetch()){
-				echo "<tr><th>".$playerName." </th> <th>  ".$points."  </th> <th>   ".$assists."  </th> <th>   ".$rebounds."</th><th>".$playtime."</th></tr>";
-			}
-		?>
-
-	</table>
-
-  </form>
-
-  <form action="gameDetail.php?gameID=<?php echo $gameID?>" method="post">
-    <h2>Discussion</h2>
+  <div class="page-content">
 
     <?php
-    // Retrieve comments on page
-    $sql = "SELECT username, description, timestamp ";
-    $sql .= "FROM comments ";
-    $sql .= "WHERE gameID = ?";
-    $stmt2=$db->prepare($sql);
-    $stmt2->bind_param('i',$gameID);
-    $stmt2->execute();
-    $stmt2->store_result();
-    $result2=$stmt2->bind_result($username, $description, $timestamp);
-
-    echo "<div class='comment-section'>";
-    while($stmt2->fetch()){
-      //echo(date("Y-m-d",$timestamp));
-
-      $date = date("Y-m-d", $timestamp);
-      echo "<div class='comment'>";
-      echo "<p><strong>$username</strong></p>";
-      echo "<p>$date</p>";
-      echo "<p>$description</p>";
-      echo "</div>";
-    }
-    echo "</div>";
-
-
-    if (!empty($_SESSION['username'])) {
-      echo "<textarea name='comment' rows='4' columns='50' placeholder='Write a comment'></textarea>";
-      echo "<input type='submit' name='submit' value='Submit'/>";
-    } else {
-      echo "<p>Please login to add a comment</p>";
-    }
-    ?>
+      while($stmt->fetch()){
+        echo "<h1 class='gamescore'>".$hometeam."    ".$hometeamScore."   :  ".$guestteamScore."    ".$guestTeam."</h1>";
+      }
+   ?>
+    <div class="gamedetail">
+    	 <form action="" method="post">
 
 
 
-  <form>
+        	<table>
+        		<tr><th class="playername">Player Name</th><th class="playerstat">Points</th><th class="playerstat">Assists</th><th class="playerstat">Rebounds</th><th class="playerstat">Playtime</th></tr>
+        		<?php
+
+        			$query2="SELECT playerName,points,assists,rebounds,playtime FROM gameDetail WHERE gameID=?";
+        			$stmt2=$db->prepare($query2);
+        			$stmt2->bind_param('i',$gameID);
+        			$stmt2->execute();
+        			$stmt2->store_result();
+        			$result2=$stmt2->bind_result($playerName,$points,$assists,$rebounds,$playtime);
+        			while($stmt2->fetch()){
+        				echo "<tr><td>".$playerName." </td> <td class='playerstat'>  ".$points."  </td> <td class='playerstat'>".$assists."  </td> <td class='playerstat'>   ".$rebounds."</td><td class='playerstat'>".$playtime."</td></tr>";
+        			}
+        		?>
+
+        	</table>
+
+        </form>
+      </div>
+      <div class="gamecomment">
+          <form action="gameDetail.php?gameID=<?php echo $gameID?>" method="post">
+          <h2>Discussion</h2>
+
+          <?php
+          // Retrieve comments on page
+          $sql = "SELECT username, description, timestamp ";
+          $sql .= "FROM comments ";
+          $sql .= "WHERE gameID = ?";
+          $stmt2=$db->prepare($sql);
+          $stmt2->bind_param('i',$gameID);
+          $stmt2->execute();
+          $stmt2->store_result();
+          $result2=$stmt2->bind_result($username, $description, $timestamp);
+
+          echo "<div class='comment-section'>";
+          while($stmt2->fetch()){
+            //echo(date("Y-m-d",$timestamp));
+
+            $date = date("Y-m-d", $timestamp);
+            echo "<div class='comment'>";
+            echo "<p><strong>$username</strong></p>";
+            echo "<p>$date</p>";
+            echo "<p>$description</p>";
+            echo "</div>";
+          }
+          echo "</div>";
+
+
+          if (!empty($_SESSION['username'])) {
+            echo "<textarea name='comment' rows='4' columns='50' placeholder='Write a comment'></textarea>";
+            echo "<input type='submit' name='submit' value='Submit'/>";
+          } else {
+            echo "<p>Please login to add a comment</p>";
+          } 
+          ?>
+
+        </form>
+    </div>
+  </div>
 
     <?php
       mysqli_close($connection);
